6281ff54a9222305f406478dd65629340e0ef7f0
tiennv1997 <tien.nv291997@gmail.com>
2022-11-15 21:38:21 +0700

    change abi generated forder

--- internal/contracts/generate_abi_code.sh
+++ internal/contracts/generate_abi_code.sh
@@ -5,4 +5,4 @@ if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then
     cd $SCRIPT_PATH
 fi
 
-abigen -abi ./abi/masterchefv2.abi -pkg contract -type MasterChefV2 -out ./masterchefv2_abi.go
+abigen -abi ./abi/masterchefv2.abi -pkg contract -type MasterChefV2 -out ./generated/masterchefv2_abi.go
